a4d23c7bbd96f545bb40b0071104dfbedddddaae
AlexanderLukashuk <upiter465@gmail.com>
2023-06-03 00:12:18 +0600

    Added IRepository

---
+++ TechStoreLibrary/Models/Repo/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TechStoreLibrary.Models.Entities;
+
+namespace TechStoreLibrary.Models.Repo
+{
+	public interface IRepository
+	{
+		IQueryable<Product> Products { get; }
+		IQueryable<Category> Categories { get; }
+	}
+}
+

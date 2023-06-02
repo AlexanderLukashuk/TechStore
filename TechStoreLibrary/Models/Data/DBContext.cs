fca713486e85e0d51f02f713094ccf6fc6acf397
AlexanderLukashuk <upiter465@gmail.com>
2023-06-03 00:18:47 +0600

    Added EFRepository, DBContext and EntityFramework

---
+++ TechStoreLibrary/Models/Data/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TechStoreLibrary.Models.Entities;
+
+namespace TechStoreLibrary.Models.Data
+{
+	public class DBContext : DbContext
+	{
+		public DBContext(DbContextOptions<DBContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Product> Products { get; set; }
+
+		public DbSet<Category> Categories { get; set; }
+	}
+}
+
---
+++ TechStoreLibrary/Models/Repo/EFRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TechStoreLibrary.Models.Entities;
+
+namespace TechStoreLibrary.Models.Repo
+{
+	public class EFRepository : IRepository
+	{
+		private DBContext context;
+
+		public EFRepository(DBContext ctx)
+		{
+			context = ctx;
+		}
+
+		public IQueryable<Product> Products => context.Products;
+
+		public IQueryable<Category> Categories => context.Categories;
+    }
+}
+

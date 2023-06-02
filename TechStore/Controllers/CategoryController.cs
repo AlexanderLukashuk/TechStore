fa798c47517af9d9a65900f7822a454edea308c2
AlexanderLukashuk <upiter465@gmail.com>
2023-06-03 00:51:13 +0600

    Added CategoryController, changed Home/Index

---
+++ TechStore/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using TechStoreLibrary.Models.Data;
+using TechStoreLibrary.Models.Repo;
+
+namespace TechStore.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly IRepository _repository;
+
+		private DBContext _dBContext;
+
+		public CategoryController(IRepository repository, DBContext context)
+		{
+			_repository = repository ?? throw new ArgumentNullException(nameof(repository));
+			_dBContext = context;
+		}
+
+		[Route("/Categories")]
+		public IActionResult Index() => View(_repository.Categories);
+	}
+}
+

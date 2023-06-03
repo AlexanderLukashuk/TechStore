0c5082455892a0d3eadf95960e1160f51ea3bfaf
AlexanderLukashuk <upiter465@gmail.com>
2023-06-03 12:24:37 +0600

    Created SeedData class and migrations

---
+++ TechStore/Models/Data/SeedData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TechStoreLibrary.Models.Data;
+using TechStoreLibrary.Models.Entities;
+
+namespace TechStore.Models.Data
+{
+	public class SeedData
+	{
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            DBContext context = app.ApplicationServices
+                .CreateScope().ServiceProvider.GetRequiredService<DBContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            if (!context.Categories.Any())
+            {
+                context.Categories.AddRange(
+                    new Category
+                    {
+                        Name = "Computers and Laptops"
+                    },
+                    new Category
+                    {
+                        Name = "Computer peripherals"
+                    },
+                    new Category
+                    {
+                        Name = "Smartphones and gadgets"
+                    },
+                    new Category
+                    {
+                        Name = "Accessories"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
